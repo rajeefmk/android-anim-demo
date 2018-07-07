3368784016fbc2a867e661e1fee0e954da5ff43d
Rajeef <rajeefmk@gmail.com>
2018-07-07 15:49:16 +0530

    v1: Basic alpha animation

--- app/src/main/java/com/rajeefmk/androidanimdemo/MainActivity.java
+++ app/src/main/java/com/rajeefmk/androidanimdemo/MainActivity.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +18,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupAlertDialog();
+        setupCrossFadeTextView();
+    }
 
+    private void setupAlertDialog() {
         Button showAlertButton = findViewById(R.id.show_alert_button);
         showAlertButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +44,40 @@ public class MainActivity extends AppCompatActivity {
         if (dialog.getWindow() != null)
             dialog.getWindow().getAttributes().windowAnimations = R.style.dialog_animation;
         dialog.show();
+
+    }
+
+    private void setupCrossFadeTextView() {
+        Switch textToggleSwitch = findViewById(R.id.toggle_switch);
+        TextView toggleText = findViewById(R.id.toggle_textview);
+
+        textToggleSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            AlphaAnimation alphaAnimation = new AlphaAnimation(1.0f, 0.0f);
+            alphaAnimation.setDuration(200);
+            alphaAnimation.setRepeatCount(1);
+            alphaAnimation.setRepeatMode(Animation.REVERSE);
+            alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+                    if (isChecked)
+                        toggleText.setText("Check text");
+                    else
+                        toggleText.setText("UnChecked text");
+                }
+            });
+            toggleText.startAnimation(alphaAnimation);
+
+        });
 
     }
 
