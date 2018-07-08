08bd3a33fe6d45b9d40dfcf9b1951154126f0651
Rajeef <rajeefmk@gmail.com>
2018-07-08 19:35:03 +0530

    feat: Add sample for Animated Vector Drawables

--- app/src/main/java/com/rajeefmk/androidanimdemo/DrawableAnimationActivity.java
+++ app/src/main/java/com/rajeefmk/androidanimdemo/DrawableAnimationActivity.java
@@ -1,9 +1,13 @@
 package com.rajeefmk.androidanimdemo;
 
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.graphics.drawable.AnimationDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 
 public class DrawableAnimationActivity extends AppCompatActivity {
@@ -15,6 +19,18 @@ public class DrawableAnimationActivity extends AppCompatActivity {
         if (getSupportActionBar() != null)
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setupAnimationListDrawable();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setupAnimatedVectorDrawable();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void setupAnimatedVectorDrawable() {
+        ImageView animatedVectorImageView= findViewById(R.id.vector_animation_imageview);
+        animatedVectorImageView.setBackgroundResource(R.drawable.sample_animated_vector_drawable);
+        AnimatedVectorDrawable animatedVectorDrawable = (AnimatedVectorDrawable) animatedVectorImageView.getBackground();
+        animatedVectorImageView.setOnClickListener(v -> animatedVectorDrawable.start());
+
     }
 
     private void setupAnimationListDrawable() {
@@ -22,9 +38,7 @@ public class DrawableAnimationActivity extends AppCompatActivity {
         animationListImageView.setBackgroundResource(R.drawable.sample_animation_list_drawable);
         AnimationDrawable animationDrawable = (AnimationDrawable) animationListImageView.getBackground();
 
-        animationListImageView.setOnClickListener(v -> {
-            animationDrawable.start();
-        });
+        animationListImageView.setOnClickListener(v -> animationDrawable.start());
     }
 
     @Override
